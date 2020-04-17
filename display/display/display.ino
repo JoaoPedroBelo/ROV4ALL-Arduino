ae8cc47427555aef14a4ae0393a3a08ba8149df6
João Pedro Vieira Belo <30899202+JoaoPedroBelo@users.noreply.github.com>
2020-04-17 18:00:59 +0100

    bugs fixed

--- display/display/display.ino
+++ display/display/display.ino
@@ -5,16 +5,7 @@
 
 // Set the LCD address to 0x27 for a 16 chars and 2 line display
 LiquidCrystal_I2C lcd(0x27, 16, 2);
-//Contantes
-//VALORES DE REFERENCIA RETIRADOS DE INUMEROS TESTES, valores do sensor
-//EIXO DO X
-#define RefSul 40
-#define RefNorte 4
-//EIXO do Y
-#define RefOeste 30
 
-// an MPU9250 object with the MPU-9250 sensor on I2C bus 0 with address 0x68
-MPU9250 IMU(Wire, 0x68);
 int status;
 
 void setup()
@@ -32,7 +23,5 @@ void loop()
   // Do nothing here...
     //lcd.print("teste!");
     lcd.setCursor(2, 5);
-      lcd.print("1");
-
-
+    lcd.print("1");
 }
